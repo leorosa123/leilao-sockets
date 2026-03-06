05824023372d32a5d2117658ae6ff53c1d6b2df7
Léo Rosa <leocamargorosa12@gmail.com>
2026-03-05 21:14:05 -0300

    trabalhando com threading simultaneamente

--- client.py
+++ client.py
@@ -1,20 +1,54 @@
 import socket
+import threading
+from time import sleep
 
 # endereço IP e porta do servidor
 HOST = "127.0.0.1"
 PORT = 5000
 
+# lock para controlar escrita no terminal
+print_lock = threading.Lock()
+
+# função para enviar mensagens ao servidor
+def enviar(sock):
+
+    while True:
+        with print_lock:
+            msg = input("Digite comando ou lance: ")
+
+        sock.send(msg.encode())
+
+        if msg == ":quit":
+            break
+        
+#função para receber mensagens do servidor
+def receber(sock):
+
+    while True:
+
+        try:
+
+            msg = sock.recv(1024).decode()
+
+            if not msg:
+                break
+            
+            with print_lock:
+                print("\nServidor:", msg)
+          
+        except:
+            break
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# se conecta ao servidor
+
 client.connect((HOST, PORT))
 
-while True:
-    # enivar uma menssagem teste ao server 
-    msg = input("Digite algo: ")
-    # envia a menssagem codificada para o servidor
-    client.send(msg.encode())
+# criando threads para enviar e receber mensagens simultaneamente
+t1 = threading.Thread(target=enviar, args=(client,))
+t2 = threading.Thread(target=receber, args=(client,))
 
-    if msg == "sair":
-        break
+t1.start()
+t2.start()
 
-client.close()
+t1.join()
+t2.join()
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 # endereço IP e porta do servidor
 HOST = "127.0.0.1"
@@ -7,7 +8,6 @@ PORT = 5000
 #criando o socket do servidor
 # conexão TCP/IP
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((HOST, PORT))
 # servidor começa a ouvir pelas conexões 
 server.listen(1)
@@ -19,13 +19,30 @@ conn, addr = server.accept()
 
 print("Cliente conectado:", addr)
 
-while True:
-    # recebendo os dados do cliente em até 1024 bytes  
-    data = conn.recv(1024).decode()
+def receber_mensagens():
 
-    if not data:
-        break
+    while True:
 
-    print("Cliente enviou:", data)
+        try:
+            # mensagem recebida do cliente, decodificada para string
+            data = conn.recv(1024).decode()
+
+            if not data:
+                break
+
+            print("Cliente enviou:", data)
+
+            conn.send(f"Servidor recebeu: {data}".encode())
+
+        except:
+            break
+
+  
+thread_receber = threading.Thread(target=receber_mensagens)
+
+thread_receber.start()
+
+# espera a thread terminar
+thread_receber.join()
 
 conn.close()
